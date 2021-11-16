4bf4910146965b47d0d6af71da99c865bee7b2db
Владимир <trn4848@mail.ru>
2021-11-16 18:44:49 +0300

    3

--- src/Main.java
+++ src/Main.java
@@ -1,46 +1,54 @@
-import java.lang.CharSequence;
+
 
 public class Main {
+    enum Label {
+        SPAM, NEGATIVE_TEXT, TOO_LONG, OK
+    }
+
+    interface TextAnalyzer {
+        Label processText(String text);
+    }
 
     public static void main(String[] args) {
 
     }
 
-    public static class AsciiCharSequence implements CharSequence {
-         final private byte[] charArray;
+    abstract class KeywordAnalyzer {
+         abstract getKeywords();
 
-        public AsciiCharSequence(byte[] ca) {
-           this.charArray = ca.clone();
 
+    }
+
+    public class SpamAnalyzer implements TextAnalyzer{
+        private String[] keywords;
+
+        public SpamAnalyzer(String[] keywords){
+            this.keywords = keywords.clone();
         }
 
-        public int length() {
-            return charArray.length;
-        }
+        Label processText(String text){
 
-        public char charAt(int index) {
-            return (char) charArray[index];
-        }
-
-        public CharSequence subSequence(int start, int end) {
-            byte[] charSubArray = new byte[end-start];
-            int index = start ;
-            for (int j = 0; j < end-start; j++) {
-                    charSubArray[j] = charArray[index];
-                    index++;
-            }
-            return new AsciiCharSequence(charSubArray);
-        }
-
-         public String toString() {
-            StringBuilder result = new StringBuilder();
-            for (int i = 0; i < charArray.length; i++) {
-                result.append((char) charArray[i]);
-            }
-            return result.toString();
         }
     }
 
+    public class NegativeTextAnalyzer implements TextAnalyzer{
+
+        Label processText(String text){
+
+        }
+    }
+
+    public class TooLongTextAnalyzer implements TextAnalyzer{
+        private int maxLength;
+
+        public TooLongTextAnalyzer(int maxLength){
+            this.maxLength = maxLength;
+        }
+
+        Label processText(String text){
+
+        }
+    }
 
 }
 
